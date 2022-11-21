1ae08a66ec82718f8fd4a6081ff55cc0dcf8e6ea
Michael Owuor <michael.owuor@gmail.com>
2022-11-21 17:56:53 +0300

    feat: layout, navbar and footer (#9)

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
 	return (
 		<ApolloProvider client={client}>
-			<Component {...pageProps} />;
+			<Component {...pageProps} />
 		</ApolloProvider>
 	);
 }
