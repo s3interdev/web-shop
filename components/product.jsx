ed21a548d4921c9103e19a6734f7c33f0b67ff15
Michael Owuor <michael.owuor@gmail.com>
2022-11-21 16:57:31 +0300

    feat: product display (#7)

--- components/product.jsx
+++ components/product.jsx
@@ -1,5 +1,26 @@
-const Product = () => {
-	return <div>Product</div>;
+/* eslint-disable @next/next/no-img-element */
+
+import Link from 'next/link';
+
+const Product = ({ product: { image, name, price, slug } }) => {
+	return (
+		<div>
+			<Link href={`/product/${slug.current}`}>
+				<div className="product-card">
+					<img
+						src={image[0].url}
+						width={250}
+						height={250}
+						className="product-image"
+						alt={name}
+					/>
+
+					<p className="product-name">{name}</p>
+					<p className="product-price">${price}</p>
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default Product;
